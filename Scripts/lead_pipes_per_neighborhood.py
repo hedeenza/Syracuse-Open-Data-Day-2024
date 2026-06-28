6db53700e082238de655a84cdb5415609810cd62
Zach Hedeen <hedeenza@gmail.com>
2026-06-28 12:22:39 -0400

    reading and filtering pipe data down to lead pipe locations

--- Scripts/lead_pipes_per_neighborhood.py
+++ Scripts/lead_pipes_per_neighborhood.py
@@ -1,5 +1,7 @@
 import geopandas as gpd
 import json
+import pandas as pd
+
 
 
 # Read in the Zip Codes & NEIGHBORHOODS GeoJSON shape-files using Geopandas
@@ -11,3 +13,16 @@ syr_nhood = gpd.read_file('../Data/Syracuse_Neighborhoods_2327829995554664018.ge
 syr_zip_json = syr_zip.to_json(drop_id = True)
 syr_nhood_json = syr_nhood.to_json(drop_id = True)
 
+
+
+# Read in the Lead Pipe Locations data; 41,126 Pipes
+pipes = pd.read_csv("../Data/Water_Services.csv")
+
+# Selecting only the Coordinates and pipe type
+pipes = pipes[['X', 'Y', 'PTYPE']]
+
+# Filtering for only the LEAD pipes; 17,143 Lead Pipes
+lead_pipes = pipes.loc[pipes['PTYPE'] == 'LEAD     ']
+
+# Rename the X and Y Columns
+lead_pipes.rename(columns = {'X':'Longitude', 'Y':'Latitude'}, inplace = True)

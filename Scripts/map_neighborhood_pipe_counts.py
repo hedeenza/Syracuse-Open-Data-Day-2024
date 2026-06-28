5915a1f012175c6235b967d14c0636eb927bdecd
Zach Hedeen <hedeenza@gmail.com>
2026-06-28 17:09:51 -0400

    adjusted base map view, added a choropleth layer to show the number of lead pipes per neighborhood

--- Scripts/map_neighborhood_pipe_counts.py
+++ Scripts/map_neighborhood_pipe_counts.py
@@ -1,11 +1,21 @@
+import geopandas as gpd
+import pandas as pd
 import folium
+import folium.plugins
 import webbrowser
+
+# Load in the Neighborhood outlines and Lead Pipe Counts by Neighborhood
+syr_nhood = gpd.read_file('../Data/Syracuse_Neighborhoods_2327829995554664018.geojson')
+# Converting the GeoJSON file to a JSON file
+syr_nhood_json = syr_nhood.to_json(drop_id = True)
+
+lead_pipes_by_neighborhood = pd.read_csv('lead_pipes_by_neighborhood.csv')
 
 # Point the map to Syracuse
 map = folium.Map(
-    location = [43.0493, -76.1455],
+    location = [43.035, -76.1455],
     tiles = None,
-    zoom_start=12
+    zoom_start = 13
 )
 
 # Set the base map parameters and add to the map
@@ -14,6 +24,27 @@ base_map = folium.FeatureGroup(name = 'Base',
                                control = False)
 folium.TileLayer(tiles = 'cartodb positron').add_to(base_map)
 base_map.add_to(map)
+
+
+
+# Add a Choropleth layer representing Lead Pipe Counts by Neighborhood
+folium.Choropleth(
+    geo_data = syr_nhood_json, 
+    name = 'Lead Pipes', 
+    data = lead_pipes_by_neighborhood, 
+    columns = ['Neighborhood', 'Count'], # specify the data frame's columns we want to use
+    key_on = 'feature.properties.Name', 
+    bins = 10,
+    fill_color = 'YlOrRd',
+    fill_opacity = 0.7,
+    line_opacity = 0.2,
+    overlay = False,
+    control = True,
+    show = True,
+    legend_name = 'Lead Pipe Locations by Neighborhood, 2023'
+).add_to(map) 
+
+
 
 # Display the map by saving it as an html, then opening it with the browser
 map.save('lead_nhood.html')
